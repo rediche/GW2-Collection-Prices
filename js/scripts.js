b6f361cabb6909c2d57afd727ca7f54811dad265
Marcus "Rediche" Geil <geil@seemslegit.dk>
2014-09-30 19:55:45 +0200

    Basic functionality, no styles

---
+++ js/scripts.js
@@ -0,0 +1,31 @@
+var root = "https://api.guildwars2.com/v2/";
+
+var items = function( collection ) {
+
+	console.log(collection);
+
+	if(collection == "hylek") {
+		var data = [14882, 14883, 14884, 14885, 14886, 14887, 14888, 14889, 14890, 14891, 14892, 14893, 14894, 14895, 14896, 14897, 14898, 14899, 14900];
+	} else if(collection == "grawl") {
+		var data = [46042, 46043, 46044, 46045, 46046, 46047, 46048, 46049, 46050, 46051, 46052, 46053, 46054, 46055, 46056, 46057, 46058, 46059, 46060]
+	}
+
+	for ( var i = 0; i < data.length; i++) {
+		$( "<div>", { id: data[i] } ).appendTo( "#" + collection + " .items");
+		console.log("Printed");
+
+		$.getJSON( root + "items/" + data[i], function( item ) {
+			$( "<img>", { src: item.icon } ).appendTo( "#" + item.id );
+			$( "<p>", { html: "Name: " + item.name } ).appendTo( "#" + item.id );
+		});
+
+		$.getJSON( root + "commerce/prices/" + data[i], function( prices ) {
+			$( "<p>", { html: "Buy: " + prices.buys.unit_price } ).appendTo( "#" + prices.id );
+			$( "<p>", { html: "Sell: " + prices.sells.unit_price } ).appendTo( "#" + prices.id );
+		});
+
+	}
+
+}
+document.write(items("grawl"));
+document.write(items("hylek"));
